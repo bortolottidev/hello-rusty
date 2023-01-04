698bd78dddeffff59307b14319b5e60fac4adb8d
bortolotti.dev <29543986+bortolottidev@users.noreply.github.com>
2023-01-04 17:30:37 -0500

    feat(guess): add error handling + add loop

--- guess_what/src/main.rs
+++ guess_what/src/main.rs
@@ -7,21 +7,32 @@ fn main() {
         .gen_range(1..=10);
 
     println!("Guess the number!");
-    println!("Please input your guess:");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+    loop {
+        println!("Please input your guess:");
 
-    println!("Your guessed: {guess}");
-    println!("The secret number is {}", secret_number);
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a valid number!");
+                continue;
+            }
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small, retry! :)"),
-        Ordering::Greater => println!("Too big, retry! :)"),
-        Ordering::Equal => println!("You win!"),
+        println!("Your guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small, retry! :)"),
+            Ordering::Greater => println!("Too big, retry! :)"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+    println!("The secret number was {}", secret_number);
 }

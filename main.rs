91d73e2e40faf34d04f85b04f8192540c941bce9
bortolotti.dev <29543986+bortolottidev@users.noreply.github.com>
2023-04-15 19:37:20 -0400

    feat(hello): add ownership fn

--- main.rs
+++ main.rs
@@ -2,6 +2,7 @@ fn main() {
     println!("Marry xmasss from rustyyyy");
     print_more();
     looping();
+    ownership();
 }
 
 fn eleven() -> u32 {
@@ -37,4 +38,23 @@ fn looping() {
     for number in (1..10).rev() {
         println!("Wow reversing {number}!");
     }
+}
+
+fn ownership() {
+    // tedious way
+    fn calc_length(str: String) -> (String, usize) {
+        let length = str.len();
+        (str, length)
+    }
+    let s1 = String::from("hello");
+    let (s2, len) = calc_length(s1);
+    // s1 is moved and i cant do this
+    // println!("{s1}");
+    println!("{s2} with len {len}");
+
+    let s3 = String::from("clone instead of mode");
+    let (s4, len2) = calc_length(s3.clone());
+    // s3 is cloned and i can do this
+    println!("{s3}");
+    println!("{s4} with len {len2}");
 }

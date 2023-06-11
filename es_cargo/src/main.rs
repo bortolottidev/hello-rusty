fccd1da37946797899eed5942477d2644368bae9
bortolotti.dev <29543986+bortolottidev@users.noreply.github.com>
2023-06-11 17:20:55 -0400

    feat(hello): more structs!

--- es_cargo/src/main.rs
+++ es_cargo/src/main.rs
@@ -1,3 +1,61 @@
+#[derive(Debug)]
+struct LoggedStructure {
+    width: u32,
+    height: u32,
+}
+
+impl LoggedStructure {
+    fn test(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> u32 {
+        self.width
+    }
+
+    fn can_hold(&self, other: &LoggedStructure) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn log(&self) -> () {
+        println!("Widht is {}, while height is {}", self.width, self.height);
+    }
+}
+
+impl LoggedStructure {
+    fn build() -> Self {
+        Self {
+            width: 1,
+            height: 2,
+        }
+    }
+}
+
 fn main() {
     println!("Cargo but PROD ready!");
+    let s = LoggedStructure {
+        width: 2,
+        height: 8,
+    };
+
+    println!("structure {:#?}", s);
+    dbg!(&s);
+    println!("test defined {}", s.test());
+    println!("test getter {}", s.width());
+
+    let bigger = LoggedStructure {
+        width: 4,
+        height: 10,
+    };
+    let smaller = LoggedStructure {
+        width: 1,
+        height: 2,
+    };
+
+    println!("s can contains bigger: {}", s.can_hold(&bigger));
+    println!("s can contains smaller: {}", s.can_hold(&smaller));
+
+    LoggedStructure::build().log();
 }
+
+
--- main.rs
+++ main.rs
@@ -75,6 +75,7 @@ struct User {
 
 struct Point(i32, i32, i32);
 
+#[derive(Debug)]
 struct UnitLikeStruct;
 
 fn play_with_struct() {
@@ -91,14 +92,21 @@ fn play_with_struct() {
     println!("Oh! What a twist! Hello {}", user.username);
 
     // hello javascript
-    let anotherUser = User { ..user };
+    let another_user = User { ..user };
     println!(
         "Hello [active] {} [user] {}",
-        anotherUser.is_active, anotherUser.username
+        another_user.is_active, another_user.username
     );
 
     // cannot use this because username has been moved (see 4.1 @borrow)
     // println!("Hello {}", user.username);
     let point = Point(11, 42, 100);
+    let point2 = Point(11, 42, 100);
+    fn distance(point1: &Point, point2: &Point) -> i32 {
+        // random computing...
+        point1.0 * point2.0
+    }
+    println!("Distance is {}", distance(&point, &point2));
     let unit = UnitLikeStruct;
+    dbg!(&unit);
 }
